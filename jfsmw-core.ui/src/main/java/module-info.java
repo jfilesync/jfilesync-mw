7c1e40c9c9991a00765f663456349f788dade4ea
Christoph Graupner <ch.graupner@workingdeveloper.net>
2018-10-28 21:41:54 +0100

    core.ui as java module

---
+++ jfsmw-core.ui/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author Christoph Graupner <ch.graupner@workingdeveloper.net>
+ */
+module jfsmw.core.ui {
+  exports net.jfilesync.jfsmw.core.ui;
+  requires java.se;
+  requires com.google.common;
+}

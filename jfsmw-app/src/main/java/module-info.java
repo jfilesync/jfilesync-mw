e5cdd3f91841f507fd218ffe0dc97843bb3ea633
Christoph Graupner <ch.graupner@workingdeveloper.net>
2018-10-29 11:46:04 +0100

    added OpenJFX JavaFx 11 dependency and run application

--- jfsmw-app/src/main/java/module-info.java
+++ jfsmw-app/src/main/java/module-info.java
@@ -2,5 +2,10 @@
  * @author Christoph Graupner <ch.graupner@workingdeveloper.net>
  */
 module net.jfilesync.jfsmw.core.app {
+  exports net.jfilesync.jfsmw.app;
+  requires net.jfilesync.jfsmw.core.ui.impl;
   requires net.jfilesync.jfsmw.core.ui;
+  requires javafx.base;
+  requires javafx.controls;
+  requires javafx.graphics;
 }
--- jfsmw-core.ui.impl/src/main/java/module-info.java
+++ jfsmw-core.ui.impl/src/main/java/module-info.java
@@ -4,5 +4,9 @@
 module net.jfilesync.jfsmw.core.ui.impl {
   exports net.jfilesync.jfsmw.core.ui.impl;
   requires net.jfilesync.jfsmw.core;
+  requires net.jfilesync.jfsmw.core.ui;
   requires com.google.common;
+  requires javafx.base;
+  requires javafx.controls;
+  requires javafx.graphics;
 }
